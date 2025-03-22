19290b0bed90507b1fe840b9aca063a8705119cd
Nedaj222 <neda.javaheri.1@ens.etsmtl.ca>
2025-03-22 19:07:32 -0400

    javascript et css login

---
+++ codeInitial/login.js
@@ -0,0 +1,16 @@
+const container = document.querySelector('.login-container');
+const btnInscrire = document.querySelector('.btn-inscrire');
+const btnConnecter = document.querySelector('.btn-connecter');
+
+//Lorsque l'utilisateur clique sur le bouton "S'inscrire" la classe active esr
+// ajoutée à la div login-container => affichage de inscription
+btnInscrire.addEventListener('click', () => {
+    container.classList.add('active');
+});
+
+//Lorsque l'utilisateur clique sur le bouton "Se connecter" la classe active est
+// supprimée de la div login-container => affichage de connexion
+btnConnecter.addEventListener('click', () => {
+    container.classList.remove('active');
+});
+
