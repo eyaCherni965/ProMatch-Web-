68bdcca36dd52b12aa9c66c2d0e806298f970a1d
elea-c <elea.charier.1@ens.etsmtl.ca>
2025-03-22 23:10:20 -0400

    fix problems

--- codeInitial/javascript/navbar.js
+++ codeInitial/javascript/navbar.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("JS chargé !");
-  
   const menuBtn = document.getElementById('menuBtn');
   const sidebar = document.getElementById('sidebar');
+  const body = document.body;
   const sidebarLinks = sidebar.querySelectorAll('a');
-  const closeBtn = document.querySelector('.close-btn');
-  
+  const closeBtn = document.getElementById('closeBtn');
+
   if (!menuBtn || !sidebar) {
     console.error("menuBtn ou sidebar introuvable !");
     return;
@@ -14,21 +13,27 @@ document.addEventListener("DOMContentLoaded", function () {
   // Ouvre/ferme la sidebar
   menuBtn.addEventListener('click', () => {
     sidebar.classList.toggle('open');
+    
+    if (sidebar.classList.contains('open')) {
+      body.classList.add('sidebar-open');
+    } else {
+      body.classList.remove('sidebar-open');
+    }
   });
 
   // Ferme la sidebar au clic sur un lien
   sidebarLinks.forEach(link => {
     link.addEventListener('click', () => {
       sidebar.classList.remove('open');
+      body.classList.remove('sidebar-open');
     });
   });
 
-  // Ferme la sidebar
+  // Ferme la sidebar au clic sur le x
   if (closeBtn) {
     closeBtn.addEventListener('click', () => {
       sidebar.classList.remove('open');
+      body.classList.remove('sidebar-open');
     });
   }
-
 });
-
