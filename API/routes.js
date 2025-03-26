eea1a8088b8e1e9124b4a05850afdb65801ed9e6
Nedaj222 <neda.javaheri.1@ens.etsmtl.ca>
2025-03-26 17:53:55 -0400

    routes modif

--- API/routes.js
+++ API/routes.js
@@ -6,15 +6,20 @@ module.exports = (db) => {
     const router = express.Router();
 
 //API connexion:
-router.get('/login', (req, res) => {
+router.post('/login', (req, res) => {
     const { email, password } = req.body;
-    const usager = testUser.find(u => u.adresseCourriel === email && u.motDePasse === password);
-    if(usager){
+    const query = `
+      SELECT * FROM Connexion WHERE courriel = ? AND mot_de_passe = ?;
+    `;
+    db.query(query, [email, password], (err, results) => {
+      if (err) return res.status(500).json({ message: 'Erreur serveur' });
+      if (results.length > 0) {
         res.send("Connexion réussie");
-    }else{
+      } else {
         res.status(401).send("Adresse courriel ou mot de passe incorrect");
-    }
-});
+      }
+    });
+  });
 
 //API inscription
 router.post('/inscription', (req, res) => {
@@ -26,15 +31,24 @@ router.post('/inscription', (req, res) => {
     if (motDePasse !== confirmerMDP) {
        return res.status(400).json({success: false, message:'Les mots de passe ne correspondent pas!'});
     }
+    const insertEmployeur = `INSERT INTO Employeur (id_employeur, nom_entreprise) VALUES (NULL, ?)`;
+    db.query(insertEmployeur, [nomEntreprise], (err, employeurResult) => {
+      if (err) return res.status(500).json({ success: false, message: 'Erreur lors de l\'ajout de l\'employeur' });
 
-    const userExistant = testUser.find(user => user.adresseCourriel === adresseCourriel);
-    if(userExistant) {
-        return res.status(400).json({success: false, message:'Adresse courriel déjà utilisée!'});
+      const id_employeur = employeurResult.insertId;
 
-    }
-    testUser.push({nom, prenom, adresseCourriel, motDePasse, nomEntreprise});
-return res.status(201).json({success: true, message:'Utilisateur créé avec succès!'});
-});
+     
+      const insertConnexion = `
+        INSERT INTO Connexion (id_connexion, courriel, mot_de_passe, id_employeur)
+        VALUES (NULL, ?, ?, ?)
+      `;
+      db.query(insertConnexion, [adresseCourriel, motDePasse, id_employeur], (err) => {
+        if (err) return res.status(500).json({ success: false, message: 'Erreur lors de l\'inscription' });
+
+        res.status(201).json({ success: true, message: 'Utilisateur créé avec succès!' });
+      });
+    });
+  });
 
 return router;
    
